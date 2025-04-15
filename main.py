7d5b20f2a65512dd15f72f94f08fb090f805412a
rakshitshah280701 <rakshitshah14@gmail.com>
2025-04-14 19:13:38 -0500

    domain link added for CORS

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://rakshitai.info"],  # Or specify: ["http://localhost:3000"]
+    allow_origins=["https://rakshitai.info", "https://www.rakshitai.info"],  # Or specify: ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
